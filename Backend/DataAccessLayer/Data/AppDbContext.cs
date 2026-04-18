ab79b45647b99628eed19ff93e8f8c3a1f411187
hemant12797 <hemantthakur8439@gmail.com>
2026-04-18 11:36:55 +0530

    Update AppDbContext.cs

--- Backend/DataAccessLayer/Data/AppDbContext.cs
+++ Backend/DataAccessLayer/Data/AppDbContext.cs
@@ -1,14 +1,14 @@
-using Microsoft.EntityFrameworkCore;
-using ModelLayer.Models;
+//using Microsoft.EntityFrameworkCore;
+//using ModelLayer.Models;
 
-namespace DataAccessLayer.Data
-{
-    public class AppDbContext: DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options): base(options){}
+//namespace DataAccessLayer.Data
+//{
+  //  public class AppDbContext: DbContext
+    //{
+      //  public AppDbContext(DbContextOptions<AppDbContext> options): base(options){}
         
-        public DbSet<QuantityMeasurement> QuantityMeasurement {get; set;}
-        public DbSet<User> Users { get; set; } 
-    }
-}
+        //public DbSet<QuantityMeasurement> QuantityMeasurement {get; set;}
+        //public DbSet<User> Users { get; set; } 
+    //}
+//}
 
